6ef5ed2460a5bcaab4781730b9be39eeba713fce
nassor01 <mohammadnassor1@gmail.com>
2026-03-11 01:20:19 +0300

    Update settings for production and commit requirements.txt

--- myproject/settings.py
+++ myproject/settings.py
@@ -20,12 +20,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&tsb*w=84+p3_c4i%_lwek14ug(fry)oiq#z_4%&x$+a8taya7'
+import os
+
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-&tsb*w=84+p3_c4i%_lwek14ug(fry)oiq#z_4%&x$+a8taya7')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['*']  # Update this with your actual domain later
+ALLOWED_HOSTS = ['*']
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
